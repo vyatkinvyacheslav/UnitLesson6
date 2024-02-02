4ffb329f52ede000d70b882f1f7dbd987ead6b7b
Vyacheslav Vyatkin <Vyatkin.vyacheslav.a@yandex.ru>
2024-02-02 12:04:05 +0500

    commit 02.02

--- src/test/java/ru/netology/web/page/TransferPage.java
+++ src/test/java/ru/netology/web/page/TransferPage.java
@@ -14,7 +14,7 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class TransferPage {
     private final SelenideElement transferButton = $("[data-test-id='action-transfer']");
-    private final SelenideElement amountInput = $("data-test-id='amount'] input");
+    private final SelenideElement amountInput = $("[data-test-id='amount'] input");
     private final SelenideElement fromInput = $("[data-test-id='from'] input");
     private final SelenideElement transferHead = $(byText("Пополнение карты"));
     private final SelenideElement errorMessage = $("[data-test-id='error-notification'] .notification__content");
